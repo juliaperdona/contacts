62a6173e4683613c3d74e620cad862b6792d1b98
juliaperdona <juliaperdona@gmail.com>
2025-04-04 10:20:32 -0300

    html inicial

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-import express from 'express'
+import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors'; 
 import rotaContatos from './routes/contatoRoutes.js'
 
 const app = express();
-app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Acess-Control-Allow-Header',
-        'Origin,, X-Requested-With, Accept ,Content-Type');
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
 
-    if(req.method === 'OPTIONS'){
-        res.header(Acess-Control-Allow-Methods, 'PATCH, POST, DELETE, GET');
-        return res.status(200).send({});
-    } 
-    next();
-})
 
 app.use('/contatos', rotaContatos)
 
 app.use((req, res, next) => {
-    const erro = new Error("Não encontrado")
-    erro.status = 404
-    next(erro)
-})
+    const erro = new Error("Não encontrado");
+    erro.status = 404; 
+    next(erro);
+});
+
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
+    console.error("Erro interno no servidor:", error);
+
+    res.status(error.status || 500);
+
     return res.send({
-        erro:{
-            mensagem: error.message
+        erro: {
+            mensagem: error.message || 'Erro interno desconhecido no servidor'
         }
-    })
-})
+    });
+});
 
 export default app;
